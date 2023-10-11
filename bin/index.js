6fb40b502b79cfe2d6bfca1d7820ef94da6d5ef7
ramil290989 <garifullinramil290989@gmail.com>
2023-10-12 01:58:31 +0500

    add fetch data

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,17 @@
 #! /usr/bin/env node
-import 'dotenv/config';
+
+import express from 'express';
+import dataGetter from '../src/dataGetter.js';
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`server starting on port ${PORT}`);
+});
+
+app.get('/api', async (request, response) => {
+  const data = await dataGetter();
+  response.json({ data });
+});
---
+++ src/dataGetter.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+const dataGetter = async () => {
+  const response = await fetch('https://api.weather.yandex.ru/v2/informers?lat=55.487308&lon=54.856422', {
+    method: 'GET',
+    headers: {
+      'X-Yandex-API-Key': process.env.WEATHER_API_KEY,
+    },
+  });
+  const result = await response.json();
+  return result;
+};
+
+export default dataGetter;
