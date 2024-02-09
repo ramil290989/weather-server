c8eb26c3af3d4ae58b41328e78829df157384261
ramil290989 <garifullinramil290989@gmail.com>
2024-02-09 21:17:57 +0500

    fix excess current

--- src/dataNormalize/getWeatherFor24Hours.js
+++ src/dataNormalize/getWeatherFor24Hours.js
@@ -8,6 +8,7 @@ const getWeatherFor24Hours = (weatherData) => {
     start: -6,
     end: 24,
   };
+  const excessLength = 6;
 
   const dateNow = new Date() / milliseconds;
   // фильтрация по промежутку времени "сейчас" + 24 часа
@@ -15,6 +16,9 @@ const getWeatherFor24Hours = (weatherData) => {
     const weatherTime = (Math.ceil((data.date.unix - dateNow) / seconds / minutes));
     return (time.start <= weatherTime && weatherTime <= time.end) && data;
   });
+  if (weatherFor24Hours.length === excessLength) {
+    weatherFor24Hours.shift();
+  }
   // добавление названий времени суток в отфильтрованный массив
   addTimesOfDay(weatherFor24Hours);
   return weatherFor24Hours;
